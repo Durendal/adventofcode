3bdd12ae885c8c4a3c8d620ff17ebc3276405ce3
Brian Hewitt <durendal@dev.skynet.lan>
2020-12-06 00:23:57 -0600

    Updates

--- problem6-2.py
+++ problem6-2.py
@@ -1,12 +1,6 @@
-def parse_group(group):
-    group = [set(i.strip()) for i in group.split("\r\n") if len(i) > 0]
-    return len(set.intersection(*group))
-
 def main():
-  data = [i for i in open('input6.txt').read().split("\r\n\r\n")]
-  groups = [parse_group(group) for group in data]
+  groups = [len(set.intersection(*[set(i.strip()) for i in group.split("\r\n") if len(i) > 0])) for group in [i for i in open('input6.txt').read().split("\r\n\r\n")]]
   print("Sum: %d" % sum(groups))
-  print("Number of groups: %d" % len(groups))
 
 if __name__ == '__main__':
   main()
--- problem6.py
+++ problem6.py
@@ -1,11 +1,6 @@
-def parse_group(group):
-    return len(set(group.replace("\r\n", "")))
-
 def main():
-  data = [i for i in open('input6.txt').read().split("\r\n\r\n")]
-  groups = [parse_group(group) for group in data]
+  groups = [len(set(group.replace("\r\n", ""))) for group in [i for i in open('input6.txt').read().split("\r\n\r\n")]]
   print("Sum: %d" % sum(groups))
-  print("Number of groups: %d" % len(data))
 
 if __name__ == '__main__':
   main()

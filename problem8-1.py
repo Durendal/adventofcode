c4ddb1a1bcfbfe81377f63a09862da76d5c49a34
Brian Hewitt <durendal@dev.skynet.lan>
2020-12-07 23:25:30 -0600

    Initial solution for problem 8 part 1

---
+++ problem8-1.py
@@ -0,0 +1,28 @@
+def parse_instruction(instruction):
+    opcode, val = instruction.split(" ")
+    return {'opcode': opcode, 'value': int(val)}
+
+def main():
+    instructions = [i.strip() for i in open('input8.txt', 'r').readlines()]
+
+    instruction_pointer = 0
+    accumulator = 0
+    history = []
+
+    while True:
+        inst = parse_instruction(instructions[instruction_pointer])
+
+        if instruction_pointer in history:
+            break
+        history.append(instruction_pointer)
+        print("Opcode: %s Value: %s IP: %d ACC: %d" % (inst['opcode'], inst['value'], instruction_pointer, accumulator))
+        if inst['opcode'] == 'acc':
+            accumulator += inst['value']
+            instruction_pointer += 1
+        elif inst['opcode'] == 'jmp':
+            instruction_pointer += inst['value']
+        elif inst['opcode'] == 'nop':
+            instruction_pointer += 1
+
+if __name__ == '__main__':
+    main()
